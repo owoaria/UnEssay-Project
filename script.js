999f18b25ecc61956c9e0c275556683dca8202b0
Aiko :3 <aikoaf@astromc.co>
2024-12-02 10:44:52 -0600

    yeah so i took an hour to figure out wtf was going on with navbar collapse. had to add 12 lines to the js and now it magically works. idk how, but if it works it works ig :D

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ $('#toggle').click(function() {
   $(this).toggleClass('is-active');
   $('#navbarCollapse').toggleClass('is-active');
 });
+// Handle navbar link click
+$('#navbarCollapse a').click(function() {
+  // Check if the navbar is open
+  if ($('#navbarCollapse').hasClass('is-active')) {
+    // Collapse the navbar and reset the toggle button
+    $('#navbarCollapse').removeClass('is-active');
+    $('#toggle').removeClass('is-active');
+  }
+});
